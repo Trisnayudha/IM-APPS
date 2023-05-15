95afeb05f7e7fb0eba973330dcb01fed65747935
Yudha <48747021+Trisnayudha@users.noreply.github.com>
2023-05-15 17:58:13 +0700

    notification

--- app/Helpers/Notification.php
+++ app/Helpers/Notification.php
@@ -2,6 +2,7 @@
 
 namespace App\Helpers;
 
+use App\Models\Notification\UsersNotification;
 use Exception;
 use Ladumor\OneSignal\OneSignal;
 
@@ -18,7 +19,10 @@ class Notification
             $fields['include_external_user_ids'] = ['external_user_id_' . $id];
 
             $notif = OneSignal::sendPush($fields, $message);
-
+            $save = new UsersNotification();
+            $save->message = $message;
+            $save->users_id = $id;
+            $save->save();
             return $this->res = $notif;
         } catch (\Exception $th) {
             return $this->res = $th->getMessage();
