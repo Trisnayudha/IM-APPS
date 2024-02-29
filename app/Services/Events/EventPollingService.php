b7ad52efc136640b61ca25bd76ae01c9cc5a42cf
Trisnayudha <trisnayuhabachtiar@gmail.com>
2024-02-29 11:36:47 +0700

    update not found polling

--- app/Services/Events/EventPollingService.php
+++ app/Services/Events/EventPollingService.php
@@ -18,21 +18,29 @@ class EventPollingService implements EventPollingInterface
         // Mencari EventPoll berdasarkan unique_id
         $eventPoll = EventsPolls::where('unique_id', $uniqueId)->first();
 
-        // Mengambil semua EventPollQuestion yang terkait dengan EventPoll, termasuk options
-        $pollQuestions = EventsPollsQuestion::with(['options'])
-            ->where('events_poll_id', $eventPoll->id)
-            ->get()
-            ->map(function ($question) {
-                return [
-                    'id' => $question->id,
-                    'type' => $question->type,
-                    'question' => $question->question,
-                    'answer' => $question->type === 'pg' ? $question->options->map(function ($option) {
-                        return ['id' => $option->id, 'pg' => $option->option_pg, 'answer' => $option->option_text];
-                    }) : null // Untuk type 'rating', 'answer' bisa di-set null atau sesuai kebutuhan
-                ];
-            });
+        if ($eventPoll) {
 
-        return $pollQuestions;
+            // Mengambil semua EventPollQuestion yang terkait dengan EventPoll, termasuk options
+            $pollQuestions = EventsPollsQuestion::with(['options'])
+                ->where('events_poll_id', $eventPoll->id)
+                ->get()
+                ->map(function ($question) {
+                    return [
+                        'id' => $question->id,
+                        'type' => $question->type,
+                        'question' => $question->question,
+                        'answer' => $question->type === 'pg' ? $question->options->map(function ($option) {
+                            return ['id' => $option->id, 'pg' => $option->option_pg, 'answer' => $option->option_text];
+                        }) : null // Untuk type 'rating', 'answer' bisa di-set null atau sesuai kebutuhan
+                    ];
+                });
+
+            return $pollQuestions;
+        } else {
+            $response['status'] = 404;
+            $response['message'] = 'Poll not Found';
+            $response['payload'] = null;
+            return response()->json($response, 200);
+        }
     }
 }
