5a3dd0aa962eefbd0dc0b03f994377b78d24db66
Yudha <yudha@indonesiaminer.com>
2025-06-04 19:54:24 +0700

    update

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -207,7 +207,7 @@ class HomeController extends Controller
                 'replay' => false,
                 'mining_directory' => $access,
                 'bookmark' => $access,
-                'speakers' => false
+                'speakers' => true
 
             ];
             $response['status'] = 200;
